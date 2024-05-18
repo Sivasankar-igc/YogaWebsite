02c9ea29f681c1168e220f068230d5e88d39c2b9
LipsaMohanty11 <lipsamohanty60234@gmail.com>
2024-05-18 12:26:11 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Pricing from './Components/Pricing'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Pricing/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
