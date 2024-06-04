3888a3a69adbdcf7a07aca70bd8a433864b9d7b9
Sivasankar-igc <sahoosivasankar33@gmail.com>
2024-06-04 20:08:47 +0530

    admin updated 2

--- Controllers/AdminControllers/adminLogin.mjs
+++ Controllers/AdminControllers/adminLogin.mjs
@@ -2,9 +2,9 @@ import adminCol from "../../Models/adminModel.mjs";
 
 export const adminLogin = async (req, res) => {
     try {
-        const { adminId, adminPassword } = req.body;
-        const response = await adminCol.findOne({ adminId: adminId, adminPassword: adminPassword });
-        response ? res.status(200).send(true) : res.status(200).send(false)
+        const { emailId, password } = req.body;
+        const response = await adminCol.findOne({ adminId: emailId, adminPassword: password });
+        response ? res.status(200).json({status:true, message:"success"}) : res.status(200).send({status:false, message:"failed"})
     } catch (error) {
         console.error(`Server Error : admin login error --> ${error}`)
     }
